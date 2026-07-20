fe1231ed52dbde1bf8ceecd804d518fcf45b6d2e
uffnv <kupn4.12oz@gmail.com>
2026-07-20 21:49:57 +0700

    Set default DATABASE_URL for easier deployment

--- bot/config.py
+++ bot/config.py
@@ -4,9 +4,7 @@ import os
 load_dotenv()
 
 BOT_TOKEN: str = os.getenv("BOT_TOKEN", "")
-DATABASE_URL: str = os.getenv("DATABASE_URL", "")
+DATABASE_URL: str = os.getenv("DATABASE_URL", "sqlite+aiosqlite:///bot/database/data.sqlite")
 
 if not BOT_TOKEN:
     raise ValueError("BOT_TOKEN не задан в .env")
-if not DATABASE_URL:
-    raise ValueError("DATABASE_URL не задан в .env")
